ac5fee6f41f320d4c359243efda89537e852782b
Odd Gunnar <oddgunnarfatland@gmail.com>
2025-10-06 15:39:37 +0200

    Legg til konfigurasjonsfiler for dev og prod miljøer, oppdater app.yaml for dynamiske verdier

--- src/server/middleware/texasMiddleware.ts
+++ src/server/middleware/texasMiddleware.ts
@@ -12,9 +12,14 @@ export const texasMiddleware = createMiddleware({ type: "function" })
     .middleware([authorizationHeaderMiddleware])
     .server(async ({ next, context }) => {
         const texasUrl = process.env["NAIS_TOKEN_EXCHANGE_ENDPOINT"];
+        const target = process.env["TILBAKEKREVING_TARGET"];
 
         if (texasUrl === undefined) {
             throw new Error("Texas URL miljøvariabel er ikke definert.");
+        }
+
+        if (target === undefined) {
+            throw new Error("Target miljøvariabel er ikke definert.");
         }
 
         const response = await fetch(texasUrl, {
@@ -22,7 +27,7 @@ export const texasMiddleware = createMiddleware({ type: "function" })
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
                 identity_provider: "azuread",
-                target: "api://dev-gcp.utenlandsadresser.tilbakekreving/.default",
+                target,
                 user_token: context.bearerToken,
             }),
         });
